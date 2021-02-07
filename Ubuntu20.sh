1fce5148b629a7a11542207abb81331641e413db
ChemicalNRG <59423696+ChemicalNRG@users.noreply.github.com>
2021-02-07 01:23:27 +0100

    Update Ubuntu20.sh

--- Ubuntu20.sh
+++ Ubuntu20.sh
@@ -87,8 +87,8 @@ cd ../..
 git clone https://code.qt.io/qt/qt5.git --branch 5.15 && cd qt5
 git submodule update --init --recursive
 mkdir build && cd build
-../configure -prefix /usr/local -opensource -confirm-license -nomake tests -nomake examples -opengl desktop -skip qtdocgallery -skip qtlocation -skip qtvirtualkeyboard -skip qtmultimedia -skip qtquickcontrols
-make -j$(($(nproc) - 2))
+../configure -prefix /usr/local -opensource -confirm-license -nomake tests -nomake examples -opengl desktop -skip qtdocgallery -skip qtlocation -skip qtvirtualkeyboard -skip qtmultimedia -skip qtquickcontrols QT_DEBUG_PLUGINS=1
+sudo make -j$(($(nproc) - 2)) 
 sudo apt remove *libopencv*-dev
 sudo apt autoremove
 sudo make install
