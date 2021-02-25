689b2a75b8d0659b524612937a4f87129d16e03d
ChemicalNRG <59423696+ChemicalNRG@users.noreply.github.com>
2021-02-25 20:20:25 +0100

    Update Ubuntu20.sh

--- Ubuntu20.sh
+++ Ubuntu20.sh
@@ -62,11 +62,14 @@ python3-pip
 sudo -H pip3 install -U jetson-stats
 
 # Compile and install the needed Modules:
+
 # libusb
 git clone https://github.com/libusb/libusb
 ./bootstrap.sh
 make
 sudo make install
+
+cd ..
 
 # zlib
 git clone https://github.com/madler/zlib
